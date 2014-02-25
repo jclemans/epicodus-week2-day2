b3759a16c68bb7128ed7fa452536a30327def8c1
Jeremy Clemans & Nolan Miller <student@epicodus.com>
2014-02-25 11:41:57 -0800

    add multi fields for address and phone and validity checks

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,42 @@
 var Contact = {
   fullName: function() {
     return this.firstName + " " + this.lastName;
+  },
+  validCheck: function() {
+    if (this.firstName === "" || this.lastName === ""){
+      return false;
+    } else {
+      return true;
+    }
   }
-}
+};
+
+var Address = {
+  fullAddress: function() {
+    return this.street + ", " + this.city + ", " + this.state;
+  },
+  validCheck: function() {
+    if (this.street === "" || this.city === "" || this.state === "") {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+var Phone = {
+  phoneNumber: function() {
+    return this.number;
+  },
+  validCheck: function() {
+   if (this.number.length < 9) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
 
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
@@ -10,35 +44,104 @@ $(document).ready(function() {
 
     var inputtedFirstName = $('input#new-first-name').val();
     var inputtedLastName = $('input#new-last-name').val();
-    var inputtedAddress = $('input#new-address').val();
+
     var newContact = Object.create(Contact);
     newContact.firstName = inputtedFirstName;
     newContact.lastName = inputtedLastName;
-    newContact.address = inputtedAddress;
+    newContact.addresses = [];
+    newContact.phones = [];
 
-    $('ul#contacts').append('<li><span class="contact">' + newContact.fullName() + '</span></li>')
+    if (newContact.validCheck()) {
+      $('ul#contacts').append('<li><span class="contact">' + newContact.fullName() + '</span></li>');
+    } else {
+      alert("Please enter a valid name.")
+    };
 
-    
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find('input.new-street').val();
+      var inputtedCity = $(this).find('input.new-city').val();
+      var inputtedState = $(this).find('input.new-state').val();
+
+      var newAddress = Object.create(Address);
+      newAddress.street = inputtedStreet;
+      newAddress.city = inputtedCity;
+      newAddress.state = inputtedState;
+
+      console.log(newAddress.validCheck());
+      console.log(newAddress.street);
+
+      if (newAddress.validCheck()) {
+        newContact.addresses.push(newAddress);   
+      } else {
+        alert("Invalid Address.");
+      }
+
+    });
+
+    $(".new-phone").each(function() {
+      var inputtedNumber = $(this).find('input.new-number').val();
+
+      var newPhoneNumber = Object.create(Phone);
+      newPhoneNumber.number = inputtedNumber;
+
+      if (newPhoneNumber.validCheck()) {
+        newContact.phones.push(newPhoneNumber);
+      } else {
+        alert("Invalid Phone Number.  Use 9 digit format.");
+      }
+
+    });
+
+
 
     $('.contact').last().click(function() {
       $('#show-contact').show();
-      $('#show-contact h2').show();
       $('#show-contact h2').text(newContact.fullName());
       $('.first-name').text(newContact.firstName);
       $('.last-name').text(newContact.lastName);
-      $('.address').text(newContact.address);
+
+      $("ul#addresses").text("");
+      $("ul#phone-numbers").text("");
+      
+      newContact.phones.forEach(function(phone) {
+        $('ul#phone-numbers').append("<li>" + phone.phoneNumber() + "</li>");
+      });
+
+      newContact.addresses.forEach(function(address) {
+        $('ul#addresses').append("<li>" + address.fullAddress() + "</li>");
+
+      });
     });
 
     this.reset();
 
   });
 
+$("#add-phone").click(function() {
+  $("#new-phones").append('<div class="new-phone">' +
+                            '<div class="form-group">' +
+                              '<label for="new-phone-number">Phone Number</label>' +
+                              '<input type="text" class="form-control new-number">' +
+                            '</div>' +
+                          '</div>');
+});
 
+$("#add-address").click(function() {
+  $("#new-addresses").append('<div class="new-address">' + 
+                               '<div class="form-group">' + 
+                                 '<label for="new-street">Street</label>' + 
+                                 '<input type="text" class="form-control new-street">' + 
+                               '</div>' + 
+                               '<div class="form-group">' + 
+                                 '<label for="new-city">City</label>' + 
+                                 '<input type="text" class="form-control new-city">' + 
+                               '</div>' + 
+                               '<div class="form-group">' + 
+                                 '<label for="new-state">State</label>' + 
+                                 '<input type="text" class="form-control new-state">' + 
+                               '</div>' + 
+                             '</div>');
+});
   
 })
 
-/*var steveIrwin = Object.create(Contact);
-steveIrwin.firstName = 'Steve';
-steveIrwin.lastName = 'Irwin';
-steveIrwin.fullName();
-*/
--- spec/specs.js
+++ spec/specs.js
@@ -6,7 +6,63 @@ describe('Contact', function() {
         testContact.lastName = 'Irwin'
         testContact.fullName().should.equal('Steve Irwin');
       });
+  });
+});
 
+describe("Address", function() {
+  describe("fullAddress", function() {
+    it("returns the full address with nice formatting", function() {
+      var testAddress = Object.create(Address);
+      testAddress.street = "208 SW 5th Ave";
+      testAddress.city = "Portland";
+      testAddress.state = "Oregon";
+      testAddress.fullAddress().should.equal("208 SW 5th Ave, Portland, Oregon");
+    });
   });
 
+  describe("validCheck", function() {
+    it("returns a boolean indicating if the entered information is valid", function() {
+      var testAddress = Object.create(Address);
+      testAddress.street = "";
+      testAddress.city = "";
+      testAddress.state = "";
+      testAddress.validCheck().should.equal(false);
+    });
+
+    it("returns a boolean indicating if the entered information is valid", function() {
+      var testAddress = Object.create(Address);
+      testAddress.street = "123";
+      testAddress.city = "portland";
+      testAddress.state = "or";
+      testAddress.validCheck().should.equal(true);
+    });
+  });
+
+
+
 });
+
+describe("Phone", function() {
+  describe("phoneNumber", function() {
+    it("returns the phone number entered with nice formatting", function() {
+      var testPhone = Object.create(Phone);
+      testPhone.number = "5032569874";
+      testPhone.phoneNumber().should.equal("5032569874");
+    });
+  });
+  describe("validCheck", function() {
+    it("returns a boolean indicating if the entered information is valid", function() {
+      var testPhone = Object.create(Phone);
+      testPhone.number = " ";
+      testPhone.validCheck().should.equal(false);
+    });
+    it("return false if the number entered is not a valid 9 digit number", function() {
+      var testPhone = Object.create(Phone);
+      testPhone.number = "12345";
+      testPhone.validCheck().should.equal(false);
+    });
+  });
+});
+
+
+
